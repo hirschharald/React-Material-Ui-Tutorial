fdaf2b2fca2ab4d9339f4ec9d72e2941e6922f0a
hirschharald <hirschharald@gmx.net>
2021-07-16 12:49:05 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>Welcome to the React Material-Ui Tutorial</h2>
+      
+    </div>
+  );
+}
